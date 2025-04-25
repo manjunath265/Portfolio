7077d7fc35cd9224ae5e60e7d523412df81121ca
manjunath265 <manjunathmanjunathreddy580@gmail.com>
2025-04-24 20:13:25 -0500

    Restore frontend code

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,8 +8,27 @@ const Hero = () => {
         <h2><span>I'm Manjunath Reddy Edla,</span> Full-Stack Developer</h2>
         <p>Master's student in Information Systems (CGPA: 3.93/4) with experience in full-stack development and data engineering. Skilled in Python, TypeScript, Java, SQL, Flask, Express, and React. Proficient in building scalable APIs, data pipelines, and cloud-native applications using AWS, Azure, Docker, and Kubernetes. Passionate about full stack, performance optimization, and delivering impactful tech solutions.</p>
         <div className="hero-action">
-            <div className = 'hero-connect'> connect with me</div>
-            <div className="hero-resume">My resume</div>
+
+            <div className = 'hero-connect'>
+              <a 
+              href= "https://www.linkedin.com/in/edlamanjunathreddy/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="connect-link"
+              >connect with me</a>
+               </div>
+
+            <div className="hero-resume">
+  <a 
+    href="http://localhost:5000/resume"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="resume-link"
+  >
+    My Resume
+  </a>
+</div>
+
         </div>
     </div>
   )
